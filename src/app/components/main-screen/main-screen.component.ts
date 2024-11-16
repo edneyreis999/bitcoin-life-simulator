99969f3bea4a682d1b0658abfcad16c73c512d43
Edney <edney.filho@canoo.com>
2024-11-16 01:45:44 -0300

    implement pause menu functionality with transfer funds feature and styling

--- src/app/components/main-screen/main-screen.component.ts
+++ src/app/components/main-screen/main-screen.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PauseMenuComponent } from '../pause-menu/pause-menu.component';
 
 @Component({
   selector: 'app-main-screen',
   standalone: true,
-  imports: [],
+  imports: [PauseMenuComponent],
   templateUrl: './main-screen.component.html',
-  styleUrl: './main-screen.component.scss'
+  styleUrl: './main-screen.component.scss',
 })
 export class MainScreenComponent {
+  @ViewChild(PauseMenuComponent) pauseMenu!: PauseMenuComponent;
 
+  openPauseMenu() {
+    this.pauseMenu.openMenu();
+  }
 }
--- src/app/components/pause-menu/pause-menu.component.ts
+++ src/app/components/pause-menu/pause-menu.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pause-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pause-menu.component.html',
-  styleUrl: './pause-menu.component.scss'
+  styleUrl: './pause-menu.component.scss',
 })
 export class PauseMenuComponent {
+  @Input() realLifeBalance = 10000; // Exemplo
+  @Input() investmentBalance = 0; // Exemplo
 
+  isVisible = false;
+  transferPercentage = 50; // Slider começa no meio
+  transferAmount = 0;
+  transactionFee = 0;
+
+  openMenu() {
+    this.isVisible = true;
+  }
+
+  closeMenu() {
+    this.isVisible = false;
+  }
+
+  updateTransferValues() {
+    const total = this.realLifeBalance + this.investmentBalance;
+    this.transferAmount = Math.round((this.transferPercentage / 100) * total);
+    this.transactionFee = Math.round(this.transferAmount * 0.02); // Ex.: 2% de taxa
+  }
 }
